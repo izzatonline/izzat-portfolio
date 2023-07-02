70df1ef9beccd994a88a68c257670c1a300a6743
izzatonline <izzat.online@gmail.com>
2023-07-03 05:04:36 +0800

    second commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,11 +10,11 @@ const Main = () => {
             <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
                 <div>
                     <p className="uppercase text-sm tracking-widest text-gray-600">
-                        Let's Build Something Together
+                        Let&apos;s Build Something Together
                     </p>
                     <h1 className="py-4 text-gray-700">
-                        Hi, I'm <span className="text-[#5651e5]">Izzat J</span>{" "}
-                        !
+                        Hi, I&apos;m{" "}
+                        <span className="text-[#5651e5]">Izzat J</span> !
                     </h1>
                     <h1 className="py-4 text-gray-700">
                         A Full-Stack Developer
