1f4e0372253fc3d47ab859d8605f2a5d60f6a7b9
izzatonline <izzat.online@gmail.com>
2023-07-03 04:53:04 +0800

    first commit

---
+++ components/About.jsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            id="about"
+            className="w-full md:h-screen p-2 flex items-center py-16"
+        >
+            <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+                <div className="col-span-2">
+                    <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+                        About
+                    </p>
+                    <h2 className="py-2 text-gray-600">Who I Am</h2>
+                    <p className="py-2 text-gray-600">Hello and Hi!</p>
+                    <p className="py-2 text-gray-600">
+                        I am an experienced Project Engineer transitioning to a
+                        career as a versatile professional in the fields of Full
+                        Stack Development and Data Analysis. Passionate about
+                        leveraging technology to drive innovation and solve
+                        complex problems, I bring a unique blend of technical
+                        expertise and project management skills to the table.
+                        With a solid foundation in engineering principles and a
+                        strong analytical mindset, I am adept at translating
+                        business requirements into practical solutions.
+                    </p>
+                    <p className="py-2 text-gray-600">
+                        Eager to apply my programming knowledge, data
+                        manipulation skills, and problem-solving abilities to
+                        contribute effectively as a Full Stack Developer or Data
+                        Analyst. Committed to continuous learning and growth, I
+                        thrive in dynamic and collaborative environments where I
+                        can make a meaningful impact.
+                    </p>
+                    <Link href="/#projects">
+                        <p className="py-2 text-gray-600 underline cursor-pointer">
+                            Check out some of my latest projects.
+                        </p>
+                    </Link>
+                </div>
+                <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+                    <Image
+                        src="/../public/assets/profilepic-izzat.jpg"
+                        alt="/"
+                        width={400}
+                        height={400}
+                        className="rounded-xl"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ components/Main.jsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const Main = () => {
+    return (
+        <div id="home" className="w-full h-screen text-center">
+            <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+                <div>
+                    <p className="uppercase text-sm tracking-widest text-gray-600">
+                        Let's Build Something Together
+                    </p>
+                    <h1 className="py-4 text-gray-700">
+                        Hi, I'm <span className="text-[#5651e5]">Izzat J</span>{" "}
+                        !
+                    </h1>
+                    <h1 className="py-4 text-gray-700">
+                        A Full-Stack Developer
+                    </h1>
+                    <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+                        Passionate about building robust and user-friendly web
+                        applications. Seeking opportunities to grow and learn in
+                        the ever-evolving world of web development.
+                        <br /> #CodeEnthusiast #WebDeveloper #TechLover
+                    </p>
+                    <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                        <Link
+                            href="https://www.linkedin.com/in/izzatjamalullail/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                                <FaLinkedinIn />
+                            </div>
+                        </Link>
+                        <Link
+                            href="https://github.com/izzatonline"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                                <FaGithub />
+                            </div>
+                        </Link>
+                        <Link href="/#contact">
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                                <AiOutlineMail />
+                            </div>
+                        </Link>
+                        <Link href="/resume">
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                                <BsFillPersonLinesFill />
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
